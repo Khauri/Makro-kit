40b98083ff33e8b37878ff7df1738a1abfe0580e
khauri <kmcclain@email.wm.edu>
2022-06-28 18:48:56 -0400

    clone example into a provided path

--- packages/create/index.js
+++ packages/create/index.js
@@ -1,5 +1,8 @@
 // Clones a starter project directory from a git repository
 const degit = require('degit');
+const path = require('path');
+const fs = require('fs');
+const {program} = require('commander');
 
 const TEMPLATE_REPO = 'Khauri/Makro-kit';
 const TEMPLATE_BASE_PATH = 'examples/default';
@@ -10,6 +13,18 @@ emitter.on('info', info => {
 	console.log(info.message);
 });
 
-emitter.clone('./test').then(() => {
-	console.log('done');
-});
+// cli program that takes in the path of the project to be created
+program.version('0.0.1')
+	.arguments('<path>')
+	.action(async (p) => {
+		// resolve the path using the cwd
+		const resolvedPath = path.resolve(process.cwd(), p);
+		// check if the resolvedPath is an empty directory
+		if (fs.existsSync(resolvedPath) && fs.readdirSync(resolvedPath).length > 0) {
+			console.error('Error: the provided path is not an empty directory.');
+			process.exit(1);
+		}
+		console.log(`Creating project at ${resolvedPath}`);
+		await emitter.clone(path);
+	})
+	.parse(process.argv);
